af87ba4c1454507247f9e21da604fd855ed0f5af
VitorTavaresO <vitortavares.o55@gmail.com>
2024-07-23 22:36:59 -0300

    Created Pages Home and Login || Created Components Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,20 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
    <>
+    <Header />
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={Home} />
         <Route path="/login" Component={Login} />
       </Routes>
-    </BrowserRouter>    
+    </BrowserRouter>   
+    <Footer /> 
    </>
   );
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
     return (
         <div className="home">
         <h1>Home</h1>
-        <Link to="/login">Login</Link>
         </div>
     );
 };
