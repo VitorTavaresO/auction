0d6f77d37a46abb21941e2061adb842b70ee6cdf
VitorTavaresO <vitortavares.o55@gmail.com>
2024-08-12 20:15:33 -0300

    feat - implementation of the layouts system with the default layout with header and footer, and the simplelayout to be used without header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,18 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
+import DefaultLayout from './components/DefaultLayout';
+import SimpleLayout from './components/SimpleLayout';
 
 function App() {
   return (
    <>
-    <Header />
     <BrowserRouter>
       <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/login" Component={Login} />
+        <Route path="/" element={<DefaultLayout> <Home /> </DefaultLayout>} />
+        <Route path="/login" element={<SimpleLayout> <Login /> </SimpleLayout>} />
       </Routes>
     </BrowserRouter>   
-    <Footer /> 
    </>
   );
 }
