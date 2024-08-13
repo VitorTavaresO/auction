1edf84025be6f52b8a719c81718797bbc1e3d85e
VitorTavaresO <vitortavares.o55@gmail.com>
2024-08-12 22:42:34 -0300

    feat - created register screen and developed form struct

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 import DefaultLayout from './components/DefaultLayout';
 import SimpleLayout from './components/SimpleLayout';
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<DefaultLayout> <Home /> </DefaultLayout>} />
         <Route path="/login" element={<SimpleLayout> <Login /> </SimpleLayout>} />
+        <Route path="/register" element={<SimpleLayout> <Register /> </SimpleLayout>} />
       </Routes>
     </BrowserRouter>   
    </>
---
+++ frontend/src/pages/register/Register.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Card } from 'primereact/card';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+import { Calendar } from 'primereact/calendar';
+import { FloatLabel } from 'primereact/floatlabel';
+import { Button } from 'primereact/button';
+import { Image } from 'primereact/image';
+import './Register.css';
+
+const Register = () => {
+    return (
+        <div className="flex align-items-center justify-content-center bg-yellow-100">
+            <Card className="m-5 sm:w-30rem md:w-40rem lg:w-50rem xl:w-60rem flex flex-column align-items-center justify-content-center text-center">
+            <Image src="./images/logo.png" alt="Logo" width="250" className="logo mb-3" />
+                <h2>Register</h2>
+                <FloatLabel className="w-full mb-3 mb-5 sm:w-25rem md:w-35rem lg:w-45rem xl:w-55rem">
+                    <InputText className="w-full" />
+                    <label htmlFor="first-name">First Name</label>
+                </FloatLabel>
+                <FloatLabel className="w-full mb-3 margin-label">
+                    <InputText className="w-full" />
+                    <label htmlFor="last-name">Last Name</label>
+                </FloatLabel>
+                <FloatLabel className="w-full mb-3 margin-label">
+                    <Calendar className="w-full" />
+                    <label htmlFor="birthday">Birthday</label>
+                </FloatLabel>
+                <FloatLabel className="w-full mb-3 margin-label">
+                    <InputText keyfilter="email" className="w-full" />
+                    <label htmlFor="email">Email</label>
+                </FloatLabel>
+                <FloatLabel className="w-full mb-3 margin-label">
+                    <InputText keyfilter="int" className="w-full" />
+                    <label htmlFor="phone">Phone</label>
+                </FloatLabel>
+                <FloatLabel className="w-full mb-3 margin-label">
+                    <Password toggleMask className="w-full" />
+                    <label htmlFor="password">Password</label>
+                </FloatLabel>
+                <FloatLabel className="w-full mb-3 margin-label">
+                    <Password toggleMask className="w-full" />
+                    <label htmlFor="confirm-password">Confirm Password</label>
+                </FloatLabel>
+                <Button label="Cancel" className="mb-4 mt-5 w-full bg-red-500 border-red-500" />
+                <Button label="Submit" className="mb-4 w-full bg-green-500 border-green-500" />
+            </Card>
+        </div>
+    );
+};
+
+export default Register;
