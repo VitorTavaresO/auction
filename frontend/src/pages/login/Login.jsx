ca4a2fc6f5cd754035353276c83757eef6f1a26d
VitorTavaresO <vitortavares.o55@gmail.com>
2024-08-12 22:42:10 -0300

    fix - logo path changed to use tha same png on every logo image

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -5,20 +5,20 @@ import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
 import { FloatLabel } from 'primereact/floatlabel';
 import { Image } from 'primereact/image';
-import logo from './logo.png';
+import 'primeflex/primeflex.css';
 
 const Login = () => {
     return (
         <div className="login flex align-items-center justify-content-center bg-yellow-100">
-            <Card className="md:w-25rem flex flex-column align-items-center justify-content-center text-center">
-                <Image src={logo} alt="Logo" width="250" className="logo mb-3" />
+            <Card className="sm:w-30rem flex flex-column align-items-center justify-content-center text-center">
+                <Image src="./images/logo.png" alt="Logo" width="250" className="logo mb-3" />
                 <h2>Login</h2>
-                <FloatLabel className="w-full mb-3 margin-label">
+                <FloatLabel className="w-full mb-5">
                     <InputText className="w-full" />
                     <label htmlFor="email">Email</label>
                 </FloatLabel>
-                <FloatLabel className="w-full mb-3 margin-label">
-                    <Password toggleMask className="w-full" />
+                <FloatLabel className="w-full mb-5">
+                    <Password feedback={false} toggleMask className="w-full p-text-lg p-p-3" />
                     <label htmlFor="password">Password</label>
                 </FloatLabel>
                 <Button label="Forget Password?" link onClick={() => window.open('#', '_blank')} className="mb-3" />
