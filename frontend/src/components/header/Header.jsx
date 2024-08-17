1fc6b7986726994cce2481cc9d61ba8b3a0fe9b4
Vitor Tavares <vitortavares.o55@gmail.com>
2024-08-17 10:30:59 -0300

    Checkpoint - Chaging branchs to style changes again

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { Menubar } from "primereact/menubar";
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <h1>Header</h1>
-        </div>
-    );
-}
+  const items = [
+    {
+      label: "Leilões",
+      icon: "pi pi-fw pi-hammer",
+      command: () => {
+        window.location = "/about";
+      },
+    },
+    {
+      label: "Home",
+      template: () => (
+        <img
+          src={"./images/logo.png"}
+          alt="Home Icon"
+          style={{ width: "150px", height: "100px" }}
+        />
+      ),
+      command: () => {
+        window.location = "/";
+      },
+    },
+    {
+      label: "Contact",
+      icon: "pi pi-fw pi-envelope",
+      command: () => {
+        window.location = "/contact";
+      },
+    },
+  ];
+  return (
+    <>
+      <Menubar
+        className="menubar-custom justify-content-center bg-gray-800 border-gray-800"
+        model={items}
+      />
+    </>
+  );
+};
 
 export default Header;
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div className="home">
-        <h1>Home</h1>
-        </div>
-    );
+  return (
+    <>
+      <div className=""></div>
+    </>
+  );
 };
 
 export default Home;
