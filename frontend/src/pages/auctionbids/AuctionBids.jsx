5f9eb5ced9bc86de7d34a185d01686154c4fed27
Vitor Tavares <vitortavares.o55@gmail.com>
2024-08-19 14:10:53 -0300

    Checkpoint - Still not Working

--- frontend/src/pages/auctionbids/AuctionBids.jsx
+++ frontend/src/pages/auctionbids/AuctionBids.jsx
@@ -7,12 +7,12 @@ const AuctionBids = () => {
   const images = [
     { src: "/images/novilha.jpg", alt: "Novilha 1" },
     { src: "/images/novilha2.jpg", alt: "Novilha 2" },
-    { src: "/images/novilha5.jpg", alt: "Novilha 3" },
+    { src: "/images/novilha3.jpg", alt: "Novilha 3" },
   ];
 
   const imageTemplate = (image) => {
     return (
-      <div>
+      <div className="product-image-container">
         <Image src={image.src} alt={image.alt} className="product-image" />
       </div>
     );
@@ -20,17 +20,20 @@ const AuctionBids = () => {
 
   return (
     <div className="min-h-screen flex justify-content-center align-items-center bg-gray-400">
-      <div className="grid sm:w-full lg:w-10 p-4">
-        <div className="col-12 flex justify-content-center">
-          <Card className="p-shadow-24">
-            <Carousel
-              value={images}
-              numVisible={1}
-              numScroll={1}
-              itemTemplate={imageTemplate}
-            />
-          </Card>
-        </div>
+      <div className="carousel-container">
+        <Card className="p-shadow-24 flex justify-content-center align-items-center">
+          <Carousel
+            value={images}
+            numVisible={1}
+            numScroll={1}
+            itemTemplate={imageTemplate}
+            circular
+            autoplayInterval={3000}
+            orientation="horizontal"
+            ViewPortHeight="360px"
+            className="custom-carousel"
+          />
+        </Card>
       </div>
     </div>
   );
