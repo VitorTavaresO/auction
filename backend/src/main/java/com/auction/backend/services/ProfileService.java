ad7d34be354eaf4313dff1c7e71a40735c6ffdb4
VitorTavaresO <vitortavares.o55@gmail.com>
2024-09-30 20:32:14 -0300

    Feat - Controller and Service

--- backend/src/main/java/com/auction/backend/services/ProfileService.java
+++ backend/src/main/java/com/auction/backend/services/ProfileService.java
@@ -1,5 +1,6 @@
 package com.auction.backend.services;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +20,21 @@ public class ProfileService {
     }
 
     public Profile read(Long id){
-        return profileRepository.findById(id).orElse(null);
+        return profileRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Profile not found"));
     }
 
     public Profile update(Profile profile){
         Profile savedProfile = profileRepository.findById(profile.getId()).orElseThrow(() -> new NoSuchElementException("Profile not found"));
         savedProfile.setName(profile.getName());
         return profileRepository.save(savedProfile);
+    }
+
+    public void delete(Long id){
+        Profile savedProfile = profileRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Profile not found"));
+        profileRepository.delete(savedProfile);
+    }
+
+    public List<Profile> list(){
+        return profileRepository.findAll();
     }
 }
